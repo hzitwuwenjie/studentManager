348475e64fcf193521eaf18456b28b4b8590d207
linbinglin <530757440>
2017-07-03 11:05:43 +0800

    提交本地仓库

---
+++ src/main/java/com/hzit/entity/Linbing.java
@@ -0,0 +1,11 @@
+package com.hzit.entity;
+
+/**
+ * Created by Administrator on 2017/7/3.
+ */
+public class Linbing {
+    public void add(){
+        int a=1;
+        System.out.println(a);
+    }
+}
