0e9459f3f28b2f173820b23e87576434cd29ce17
chnechaochang <785158717@qq.com>
2017-07-03 15:41:52 +0800

    陈超嫦编程题第2题

--- src/main/java/com/hzit/entity/ChenChaoChang.java
+++ src/main/java/com/hzit/entity/ChenChaoChang.java
@@ -1,5 +1,7 @@
 package com.hzit.entity;
 
+import java.util.Scanner;
+
 /**
  * Created by Administrator on 2017/7/3.
  */
@@ -8,7 +10,33 @@ public class ChenChaoChang {
          System.out.println("陈超嫦的GIT上传练习");
          System.out.println("修改版本");
      }
-    /*
-    *  修改
-    * */
-}
+    public boolean zhishu(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public long zhishuNum(int n){
+        int num=0;
+        int i=0;
+        int j=2;
+        while(i<n){
+           if(zhishu(j)){
+               num=num+j;
+               i++;
+           }
+            j++;
+        }
+        return num;
+    }
+
+
+    public static void main(String[] args) {
+        ChenChaoChang c=new ChenChaoChang();
+     //   System.out.println( c.zhishu(25));
+        System.out.println(c.zhishuNum(4));
+    }
+ }
