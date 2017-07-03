d447d19256972849f60d6486e1d9435e66239977
shinyrao <654594927@qq.com>
2017-07-03 14:33:55 +0800

    Merge branch 'master' of D:\idea_workspace\studentManager with conflicts.

--- src/main/java/com/hzit/entity/TonyMin.java
+++ src/main/java/com/hzit/entity/TonyMin.java
@@ -43,6 +43,7 @@ public class TonyMin {
         System.out.println("毛璇提交了代码");
         System.out.println("许克兴修改了代码");
         System.out.println("劉江文還是沒有修改代码！。。。。。。");
-        System.out.println("应传富修改了代码");
+        System.out.println("舒张刚刚修改了代码");
+
     }
 }
