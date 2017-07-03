bbcb79e1d9bf8667d23b6b2932e4f749cf18a3d3
3419037 <275646812@qq.com>
2017-07-03 14:29:57 +0800

    添加了N条代码

--- src/main/java/com/hzit/entity/TonyMin.java
+++ src/main/java/com/hzit/entity/TonyMin.java
@@ -44,5 +44,6 @@ public class TonyMin {
         System.out.println("许克兴修改了代码");
         System.out.println("劉江文還是沒有修改代码！。。。。。。");
         System.out.println("应传富修改了代码");
+        System.out.println("张武斌提交了N条代码！");
     }
 }
