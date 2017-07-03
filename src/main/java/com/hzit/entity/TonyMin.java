77e01c4c291c2dc3dee8349a862fdf5686b45827
wjljw <979201883@qq.com>
2017-07-03 14:18:40 +0800

    Merge branch 'master' of https://github.com/hzitwuwenjie/studentManager
    # Conflicts:
    #	src/main/java/com/hzit/entity/TonyMin.java

--- src/main/java/com/hzit/entity/TonyMin.java
+++ src/main/java/com/hzit/entity/TonyMin.java
@@ -35,5 +35,6 @@ public class TonyMin {
         t.exchange2(a, b);
         System.out.println("闵老师刚刚提交了代码。");
         System.out.println("汤宇航刚刚修改代码！");
+        System.out.println("劉江文又沒有修改代码！");
     }
 }
