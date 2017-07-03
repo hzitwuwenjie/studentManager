9e5eb7ccd05a04341153584a4d60c46d6907fec7
hzit-M2LF6 <245160096@qq.com>
2017-07-03 11:58:13 +0800

    YingChuanFu

---
+++ src/main/java/com/hzit/entity/YingChuanFu.java
@@ -0,0 +1,10 @@
+package com.hzit.entity;
+
+/**
+ * Created by hzit-M2LF6 on 2017/7/3.
+ */
+public class YingChuanFu {  //实体类
+    public static void main(String[] args) {
+        System.out.println("YingChuanFu");
+    }
+}
