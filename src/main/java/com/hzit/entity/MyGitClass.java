f42ab7ee4e0245fe6a06d45fa4ba4aa746b6526f
chnechaochang <785158717@qq.com>
2017-07-03 10:32:07 +0800

    本地保存一下

---
+++ src/main/java/com/hzit/entity/MyGitClass.java
@@ -0,0 +1,10 @@
+package com.hzit.entity;
+
+/**
+ * Created by Administrator on 2017/7/3.
+ */
+public class MyGitClass {
+    public void myGit(){
+        System.out.println("this is my first git class");
+    }
+}
