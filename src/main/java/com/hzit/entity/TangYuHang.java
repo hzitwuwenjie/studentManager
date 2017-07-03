5c42de72a26261a18ce87d2c2af390c6a236ca88
yuhang78 <648453648@qq.com>
2017-07-03 11:52:52 +0800

    试试

--- src/main/java/com/hzit/entity/TangYuHang.java
+++ src/main/java/com/hzit/entity/TangYuHang.java
@@ -4,8 +4,11 @@ package com.hzit.entity;
  * Created by Administrator on 2017/7/3.
  */
 public class TangYuHang {
+
+
     public void Test(){
-        System.out.println("试试看，万一成功了呢！^_^");
+        System.out.println("测试");
     }
+
 
 }
