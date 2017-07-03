5975e4d75a611c11e0d2017f2795797b380f96af
wjljw <979201883@qq.com>
2017-07-03 11:14:41 +0800

    liujiangwen

---
+++ src/main/java/com/hzit/entity/LiuJiangWen.java
@@ -0,0 +1,10 @@
+package com.hzit.entity;
+
+/**
+ * Created by Administrator on 2017/7/3 0003.
+ */
+public class LiuJiangWen {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
