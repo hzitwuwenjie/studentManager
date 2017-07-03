7cf3930a47cbc7b44ae01056d588d13c913dd5ce
THINK <1302981271@qq.com>
2017-07-03 11:46:53 +0800

    TonyMin 修改后的内容。

--- src/main/java/com/hzit/entity/TonyMin.java
+++ src/main/java/com/hzit/entity/TonyMin.java
@@ -5,6 +5,10 @@ package com.hzit.entity;
  */
 public class TonyMin {
 
+    public  void test(String s){
+        System.out.println(s);
+    }
+
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             System.out.println("中文哟：" + i);
