feafd7ef24da99db4d8c815760dff652fc06574d
TonyMin <1302981271@qq.com>
2018-07-11 10:57:56 +0800

    this is a just test....

---
+++ src/main/java/com/hzit/entity/TestGitHub.java
@@ -0,0 +1,11 @@
+package com.hzit.entity;
+
+/**
+ * Created by tony on 2018/7/11.
+ */
+public class TestGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("this is my git hub....");
+    }
+}
